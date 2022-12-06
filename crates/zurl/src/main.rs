9ef1afe22105c199fff4c90984508e823c6b4efb
iamwwc <qaq1362211689@gmail.com>
2022-12-06 12:47:00 +0800

    1

--- crates/zurl/src/main.rs
+++ crates/zurl/src/main.rs
@@ -1,3 +1,18 @@
+use clap::{command, Arg, value_parser};
+
 fn main() {
+
+    let matches = command!()
+    .arg(
+        // 默认编译时include配置，运行时可覆盖，从文件路径中加载
+        Arg::new("c")
+            .long("cipher")
+            .required(true)
+            .value_parser(value_parser!(String)),
+    )
+    .get_matches();
+
+    let mut builder = tokio::runtime::Builder::new_multi_thread();
+    let rt = builder.enable_all().build().unwrap();
     println!("Hello, world!");
 }
